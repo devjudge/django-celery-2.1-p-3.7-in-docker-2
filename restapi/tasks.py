94bf50155b0db50f5c69efe8c1e863df7e1c329b
Simar Singh <simar@codejudge.io>
2020-08-10 13:54:49 +0530

    remove celery configuration

--- restapi/tasks.py
+++
@@ -1,9 +0,0 @@
-from celery import shared_task
-import logging
-from cjapp.celery import app
-logger = logging.getLogger(__name__)
-
-@app.task
-def execute_run():
-    logger.info("Executing run-submission task for id:")
-    return "Sample Celery"
