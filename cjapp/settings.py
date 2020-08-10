ca08ccfdc8fdd672ca690cbf786b2093253d0f87
Simar Singh <simar@codejudge.io>
2020-08-10 17:05:49 +0530

    revert db

--- cjapp/settings.py
+++ cjapp/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'cjapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db',
-        'USERNAME': 'root',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': 3306
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
